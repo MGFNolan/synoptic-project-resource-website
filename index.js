6911e1b57a2adf49538f4c0792b1f9c6de605ad6
Mattn <mattn@Esthers-Air.broadband>
2022-02-18 22:57:00 +0000

    Add URL Validation

--- index.js
+++ index.js
@@ -8,29 +8,36 @@ const ratingInput = document.querySelector('#ratingInput')
 const tagInput = document.querySelector('#tagInput')
 const descInput = document.querySelector('#descInput')
 
+//URL EXTRACTION
 const getParam = (url) => {
     let parameter = new URL(url)
     return parameter.hostname;
 }
 
+//ADDING A RESOURCE
 addResourceBtn.addEventListener('click', () => {
+    const url = urlInput.value;    
     const source = srcName.value;
-    const url = urlInput.value;
     const rating = ratingInput.value;
     const tag = tagInput.value;
     const description = descInput.value;
-    rowNum += 1;
-
-    const template = 
-    `<tr> 
-        <th scope="row">${rowNum}</th>
-        <td>${source}</td>
-        <td><a href="${url} target="_blank">${getParam(url)}</a></td>
-        <td>${rating}</td>
-        <td>${tag}</td>
-        <td>${description}</td>
-    </tr>
-    `
+    let valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url)
     
-    resourceTable.innerHTML += template;
+    //URL VALIDATION
+    if(valid) {
+        rowNum += 1;
+
+        const template = 
+        `<tr> 
+            <th scope="row">${rowNum}</th>
+            <td>${source}</td>
+            <td><a href="${url} target="_blank">${getParam(url)}</a></td>
+            <td>${rating}</td>
+            <td>${tag}</td>
+            <td>${description}</td>
+        </tr>
+        `
+
+        resourceTable.innerHTML += template;
+        } alert("This URL is invalid. Try copy & pasting the URL.")
 })
