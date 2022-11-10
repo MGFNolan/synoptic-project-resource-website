32642c2c74f2b1d054464a38d690e3cfed7f3645
Matthew N <mn.gf@outlook.com>
2022-11-10 16:02:21 +0000

    hardcoding examples

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 //const { readFileSync } = require('fs');
 const bodyParser = require("body-parser");
-var cors = require('cors');
-//const { readSrcDb } = require('./database');
+var cors = require("cors");
+const { addSrcDb } = require("./database");
 const port = 3000;
 
-//const homePage = readFileSync('./index.html')
-
-// app.get('/', (req, res) => {
-//   const url = req.url
-
-//   if (url === 'http://localhost:3000/') {
-//     res.writeHead(200, { 'content-type': 'text/html' })
-//     readFileSync.write(homePage)
-//     res.end()
-//   }
-//   res.send('Hello World!')
-// })
-
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.get('/cat', (req, res) => {
-  //const url = req.url
+// app.post("/item", (req, res) => {
+//   //res.writeHead(200, { 'content-type': 'text/json' })
+//   addSrcDb("one", "two", "three", "four", "five");
+//   console.log(req);
+//   res.send(req.body);
+//   res.end();
+// });
 
-  //if (url === 'http://localhost:3000/cat') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.end()
-  //}
-  res.send('Hello World!')
-})
-
-app.post('/item', (req, res) => {
-  //res.writeHead(200, { 'content-type': 'text/json' })
-  console.log(req)
-  res.send(req.body)
-  res.end()
-})
-
-app.get('/list', (req, res) => {
-  res.send({"item":{"source":"fCC"}})
-})
-
+app.get("/list", (req, res) => {
+  res.send({
+    sources: [
+      {
+        sourcename: "freeCodeCamp",
+        url: "https://www.youtube.com/watch?v=qw--VYLpxG4&t=11100s",
+        rating: 5,
+        tags: ["this", "tag", "here"],
+        description: "fantastic. Yes. Great",
+      },
+      {
+        sourcename: "the bomb dot com",
+        url: "https://www.thebombdotcom.com",
+        rating: 5,
+        tags: ["the", "bomb", "dotcom"],
+        description: "this is great. The bomb-dot-com",
+      },
+    ],
+  });
+});
--- index.js
+++ index.js
@@ -1,61 +1,69 @@
 // const database = require("./database.js");
 
-const addResourceBtn = document.querySelector('#addResourceBtn');
-const tabBody = document.querySelector('#tableBody');
-const resourceTab = document.querySelector('#resourceTable');
+const addResourceBtn = document.querySelector("#addResourceBtn");
+const tabBody = document.querySelector("#tableBody");
+const resourceTab = document.querySelector("#resourceTable");
 let rowNum = tabBody.childElementCount;
 
-const srcName = document.querySelector('#srcInput');
-const urlInput = document.querySelector('#urlInput');
-const ratingInput = document.querySelector('#ratingInput');
-const tagInput = document.querySelector('#tagInput');
-const descInput = document.querySelector('#descInput');
+const srcName = document.querySelector("#srcInput");
+const urlInput = document.querySelector("#urlInput");
+const ratingInput = document.querySelector("#ratingInput");
+const tagInput = document.querySelector("#tagInput");
+const descInput = document.querySelector("#descInput");
 
 //URL EXTRACTION
 const getParam = (url) => {
-    let parameter = new URL(url)
-    return parameter.hostname;
-}
+  let parameter = new URL(url);
+  return parameter.hostname;
+};
 
 //ADDING A RESOURCE
-addResourceBtn.addEventListener('click', (e) => {
-    
-    // prevent default form submit if valid, otherwise, not prevent default behaviour so the HTML5 validation behaviour can take place
-    if(document.getElementById("addSourceForm").checkValidity()){
-        e.preventDefault();
-        const url = urlInput.value;    
-        const source = srcName.value;
-        const rating = ratingInput.value;
-        const tag = tagInput.value;
-        const description = descInput.value;
-        let valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url)
+addResourceBtn.addEventListener("click", (e) => {
+  // prevent default form submit if valid, otherwise, not prevent default behaviour so the HTML5 validation behaviour can take place
+  if (document.getElementById("addSourceForm").checkValidity()) {
+    e.preventDefault();
+    const url = urlInput.value;
+    const source = srcName.value;
+    const rating = ratingInput.value;
+    const tag = tagInput.value;
+    const description = descInput.value;
+    let valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
 
-        //Wouldn't it be great to run this validation onBlur? Might be an option...
-        //URL VALIDATION
-        if(valid) {
-            rowNum += 1; //you'd normally get this ID from the DB
-            //addSrcDb(source, url, rating, tag, description);
-            const template = nonEditableRow(rowNum, source, url, rating, tag, description);
-            tabBody.innerHTML += template;
-            document.getElementById("addSourceForm").reset(); //reset the fields in the form
-            } 
-        else { 
-            //change text inside modal dialog
-            document.getElementById('errText').innerHTML = "This URL is invalid. Try copy & pasting the URL.";
-            //create modal 
-            var errorModal = new bootstrap.Modal(document.getElementById('errModal'));
-            //show the modal
-            errorModal.show();
-        }
+    //Wouldn't it be great to run this validation onBlur? Might be an option...
+    //URL VALIDATION
+    if (valid) {
+      rowNum += 1; //you'd normally get this ID from the DB
+      //addSrcDb(source, url, rating, tag, description);
+      const template = nonEditableRow(
+        rowNum,
+        source,
+        url,
+        rating,
+        tag,
+        description
+      );
+      tabBody.innerHTML += template;
+      document.getElementById("addSourceForm").reset(); //reset the fields in the form
+    } else {
+      //change text inside modal dialog
+      document.getElementById("errText").innerHTML =
+        "This URL is invalid. Try copy & pasting the URL.";
+      //create modal
+      var errorModal = new bootstrap.Modal(document.getElementById("errModal"));
+      //show the modal
+      errorModal.show();
     }
-})
+  }
+});
 
 //we have split this out as we'll be reusing it
-function nonEditableRow(rowNum, source, url, rating, tag, description){
-    return `<tr data-row-id="${rowNum}"> 
+function nonEditableRow(rowNum, source, url, rating, tag, description) {
+  return `<tr data-row-id="${rowNum}"> 
     <td id="rn_${rowNum}">${rowNum}</td>
     <td id="src_${rowNum}">${source}</td>
-    <td id="url_${rowNum}"><a href="${url}" target="_blank">${getParam(url)}</a></td>
+    <td id="url_${rowNum}"><a href="${url}" target="_blank">${getParam(
+    url
+  )}</a></td>
     <td id="rtg_${rowNum}">${rating}</td>
     <td id="tag_${rowNum}">${tag}</td>
     <td id="desc_${rowNum}">${description}</td>
@@ -66,8 +74,8 @@ function nonEditableRow(rowNum, source, url, rating, tag, description){
 
 //this is our editable row, we just need to pass the parameters to it
 //QUESTION - HOW DO WE DO VALIDATION ON EDITING?????
-function editableRow(rowNum, source, url, rating, tag, description){
-    return `<tr data-row-id="${rowNum}"> 
+function editableRow(rowNum, source, url, rating, tag, description) {
+  return `<tr data-row-id="${rowNum}"> 
     <td>${rowNum}</td>
     <td><input type="text" class="form-control">${source}</input></td>
     <td><input type="text" class="form-control">${url}</input></td>
@@ -80,47 +88,56 @@ function editableRow(rowNum, source, url, rating, tag, description){
 }
 
 //REMOVE OR EDIT A SOURCE
-resourceTab.addEventListener('click', (e) => {
-    //CHANGED to use the element id rather than class
-    //because id is supposed to be unique, we could always append the
-    //rowNum to the ID, and check with "startsWith" rather than ==
-    //but this is good enough for now
+resourceTab.addEventListener("click", (e) => {
+  //CHANGED to use the element id rather than class
+  //because id is supposed to be unique, we could always append the
+  //rowNum to the ID, and check with "startsWith" rather than ==
+  //but this is good enough for now
 
-    const btn = e.target;
+  const btn = e.target;
 
-    switch(e.target.getAttribute("id")){
-        case 'delResourceBtn':
-            btn.closest('tr').remove();
-            rowNum -= 1;
-            break;
-        case 'editResourceBtn':
-            var fullRow = btn.closest('tr');
-            var rowId = fullRow.getAttribute('data-row-id'); //data attributes are very useful...
-            btn.closest('tr').innerHTML = editableRow(rowId,"","","","",""); //need to populate with the right values we get from the row....
-            break;
-        case 'cancelResourceBtn':
-            //HOW DO WE CANCEL? DO WE NEED TO STORE THE DATA FROM EDIT AND REVERT?
-            //QUESTIOn - WHAT HAPPENS IF WE HAVE MANY ROWS EDITING AT THE SAME TIME?.....
-            break;
-        case 'saveResourtceBtn':
-            //NEED TO SAVE HERE
-            break;
-        default:
-            return;
-    }
-})
+  switch (e.target.getAttribute("id")) {
+    case "delResourceBtn":
+      btn.closest("tr").remove();
+      rowNum -= 1;
+      break;
+    case "editResourceBtn":
+      var fullRow = btn.closest("tr");
+      var rowId = fullRow.getAttribute("data-row-id"); //data attributes are very useful...
+      btn.closest("tr").innerHTML = editableRow(rowId, "", "", "", "", ""); //need to populate with the right values we get from the row....
+      break;
+    case "cancelResourceBtn":
+      //HOW DO WE CANCEL? DO WE NEED TO STORE THE DATA FROM EDIT AND REVERT?
+      //QUESTIOn - WHAT HAPPENS IF WE HAVE MANY ROWS EDITING AT THE SAME TIME?.....
+      break;
+    case "saveResourtceBtn":
+      //NEED TO SAVE HERE
+      break;
+    default:
+      return;
+  }
+});
 
-async function test(){
-    console.log("Hello")
-    const response = await fetch('http://localhost:3000/item', {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            item: "test",
-            item2: "test2",
-            itemInFrontOfMe: "Slinky"
-        })
-    })
-    const text = await response.json()
-    console.log(text)
+async function test() {
+  console.log("Hello");
+  const response = await fetch("http://localhost:3000/item", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      item: "test",
+      item2: "test2",
+      itemInFrontOfMe: "Slinky",
+    }),
+  });
+  const text = await response.json();
+  console.log(text);
 }
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const response = await fetch("http://localhost:3000/list", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  const text = await response.json();
+  console.log(text);
+});
